aebaa07b91c5b0ce623837bf3b8674815c540b73
ertayx <ertaiesen05@gmail.com>
2023-11-15 20:12:02 +0600

    deleter & dunder_methods of property

--- OOP/encapsulation.py
+++ OOP/encapsulation.py
@@ -18,7 +18,7 @@ class A:
     def __private_func(self):pass
 
 a = A()
-print(a._protected) # мы не должны обращаться к защ.аттрибуту или методу через объект (на уровне соглашения)
+# print(a._protected) # мы не должны обращаться к защ.аттрибуту или методу через объект (на уровне соглашения)
 # print(a.__private) # к приватному аттрибуту я не могу обратится через объект и дочерних классов
 # _<название класса>__<название метода(аттр.)> (так нельзя на уровне соглашения)
 
@@ -27,9 +27,9 @@ class B(A):
         self.public
         # self.__private_func()
 
-b = B()
-b.func()
-print(b._A__private) # так нельзя на уровне соглашения
+# b = B()
+# b.func()
+# print(b._A__private) # так нельзя на уровне соглашения
 
 class Person:
     name = 'Nikita'
@@ -41,11 +41,11 @@ class Person:
     def set_last_name(self, new_last_name):
         self.__last_name = new_last_name
     
-human = Person()
-# human._Person__last_name = 'Zhoroev'
-print(human.get_last_name())
-human.set_last_name('Zhoroev')
-print(human.get_last_name())
+# human = Person()
+# # human._Person__last_name = 'Zhoroev'
+# print(human.get_last_name())
+# human.set_last_name('Zhoroev')
+# print(human.get_last_name())
 
 # написать класс Game в котором есть приватный аттрибут level
 # добавить в класс Game возможность получать и изменять аттрибут level
@@ -63,22 +63,23 @@ class Game:
         return self.__level
     
     @level.setter # позволяет изменить аттрибут
+    # __setattr__ - 
     def level(self, n):
         self.__level = n
 
-game = Game()
+# game = Game()
 # propety | level.getter
-print(game.get_level())
-game.set_level(90)
-print(game.get_level())
-print(game._Game__level)
-game._Game__level = 89
-print(game._Game__level)
-print(game.level)
-game.level = 90
-print(game.level)
-game.level = 78
-print(game.level)
+# print(game.get_level())
+# game.set_level(90)
+# print(game.get_level())
+# print(game._Game__level)
+# game._Game__level = 89
+# print(game._Game__level)
+# print(game.level)
+# game.level = 90
+# print(game.level)
+# game.level = 78
+# print(game.level)
 
 
 # написать класс Human который принимает чз экземпляр следующие аттрибуты: name, age, last_name
@@ -86,25 +87,109 @@ print(game.level)
 # превратить age в приватный аттрибут
 # добавить возможность получать и изменять приватный аттрибут(getter & setter)
 
-class Human:
-    def __init__(self, name, age, last_name) -> None:
-        self._name = name
-        self.__age = age
-        self.last_name = last_name
+# class Human:
+#     def __init__(self, name, age, last_name) -> None:
+#         self._name = name
+#         self.__age = age
+#         self.last_name = last_name
     
-    @property
-    def age(self):
-        return self.__age
+#     @property
+#     def age(self):
+#         return self.__age
     
-    @age.setter
-    def age(self, n):
-        self.__age = n
+#     @age.setter
+#     def age(self, n):
+#         self.__age = n
 
-human = Human('name', 78, 'last_name')
-print(human.age)
-human.age += 1
-print(human.age)
+# human = Human('name', 78, 'last_name')
+# print(human.age)
+# human.age += 1
+# print(human.age)
 
 # _create_user
 # create_user
 # create_superuser
+
+class A:
+    _a = 7
+    # getter - переопределяет маг.метод __getattribute__
+    # setter - переопределяет маг.метод __setattr__
+    # deleter - переопределяет маг.метод __delattr__
+
+# a = A()
+# print(a._a)
+# print(a.__getattribute__('_a'))
+# a._a = 5
+# a.__setattr__('_a', 5)
+# print(a.__getattribute__('_a'))
+# a.__setattr__('a', 6)
+# print(a._a)
+
+class Human:
+    def __init__(self, name) -> None:
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.deleter # срабатывает когда пишем оператор "del"
+    def name(self):
+        del self.__name
+
+human = Human('Nikita')
+print(human.name)
+del human.name
+
+# class Temp
+# target_temp
+# default_temp
+# default_temp = 7
+# target_temp = 20
+# add_temp - добавляет в default_temp + 1
+# minus_temp - отнимает с default_temp - 1
+# вы дошли до планновой задачи
+# get_temp - получает default_temp
+
+class Temp:
+    def __init__(self, target_temp) -> None:
+        self.__target_temp = target_temp
+        self.__default_temp = 10
+        self.doiti_do_target_temp()
+    
+    def add_temp(self):
+        self.__default_temp += 1
+        if self.__default_temp == self.__target_temp:
+            return 'вы дошли до планновой задачи'
+
+    def minus_temp(self):
+        if self.__default_temp == self.__target_temp:
+            return 'вы дошли до планновой задачи'
+        self.__default_temp -= 1
+
+    def get_temp(self):
+        return f'{self.__default_temp} - {self.count} вызвали метод {self.method}'
+    
+    def doiti_do_target_temp(self):
+        self.count = 0
+        if self.__target_temp>self.__default_temp:
+            for i in range(self.__target_temp-self.__default_temp):
+                self.add_temp()
+                self.method = self.add_temp.__name__
+                self.count+=1
+        elif self.__target_temp<self.__default_temp:
+            for i in range(abs(self.__target_temp-self.__default_temp)):
+                self.minus_temp()
+                self.method = self.minus_temp.__name__
+                self.count+=1
+        else:
+            self.count = 0
+            self.method = 'не воспользовались метод'
+            
+temp = Temp(7)
+# temp.default_temp = 10
+print(temp.get_temp()) # self.__default_temp - 10 вызвали метод add_temp
+# вы дошли до планновой задачи
+# print(temp.add_temp())
+
+# 7-10 = -3
